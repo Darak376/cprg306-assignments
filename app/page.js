dc2dfe5f8f716582415bac53472d4fbdfbf089c8
Darak376 <130400141+Darak376@users.noreply.github.com>
2024-03-13 15:46:29 -0600

    push

--- app/page.js
+++ app/page.js
@@ -21,5 +21,8 @@ export default function HomePage() {
                 <Link href="/week-6" className="bg-cyan-900 text-black font-bold py-5 px-7 text-yellow-300 transition duration-200">
                   Week6
                 </Link>
+                <Link href="/week-7" className="bg-cyan-900 text-black font-bold py-5 px-7 text-yellow-300 transition duration-200">
+                  Week7
+                </Link>
       </main>
     );}
---
+++ app/week-7/page.js
@@ -0,0 +1,40 @@
+"use client";
+import React, { useState } from "react";
+import ItemList from "./item-list";
+import NewItem from "./new-item";
+import itemsData from "./items.json";
+import MealIdeas from "./meal-ideas";
+
+function Page() {
+  const [items, setItems] = useState(itemsData);
+  const [selectedItemName, setSelectedItemName] = useState(null);
+
+  const handleAddItem = (newItem) => {
+    setItems((prevItems) => [...prevItems, newItem]);
+  };
+
+  const handleItemSelect = (selectedItem) => {
+    const cleanedItemName = selectedItem.name
+      .replace(/ 🥛|🍞|🥚|🍌|🥦|🍗|🍝|🧻|🍽️|🧼|/g, "")
+      .split(",")[0];
+
+    setSelectedItemName(cleanedItemName);
+  };
+
+  return (
+    <main className="bg-blue-300 flex justify-between">
+      <div className="w-1/2 p-6">
+        <h1 className="text-4xl font-bold text-yellow-400 mb-6">
+          Shopping List
+        </h1>
+        <NewItem onAddItem={handleAddItem} />
+        <ItemList items={items} onItemSelect={handleItemSelect} />
+      </div>
+      <div className="w-1/2 p-6">
+        <MealIdeas ingredient={selectedItemName} />
+      </div>
+    </main>
+  );
+}
+
+export default Page;
